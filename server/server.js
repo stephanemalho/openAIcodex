ec0a7d88f5ddf9ce6ff1848623f208f0e7f0351d
stephane.malho <stepahen.malho@gmail.com>
2023-06-04 23:43:38 +0200

    ✨ F02 remove log

--- server/server.js
+++ server/server.js
@@ -9,8 +9,6 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-console.log(process.env.OPENAI_API_KEY);
-
 const openai = new OpenAIApi(configuration);
 
 const app = express()
